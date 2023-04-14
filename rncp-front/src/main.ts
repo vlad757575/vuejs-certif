cffa3b72d069fffbd2758338b64df8e89b96c9ec
Vladimir Lavrentiev <lavren_v@etna-alternance.net>
2023-04-14 09:49:07 +0200

    install bootstrap and implement navbar

--- rncp-front/src/main.ts
+++ rncp-front/src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
 import './assets/main.css'
 
 const app = createApp(App)
--- rncp-front/src/views/HomeView.vue
+++ rncp-front/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import NavBar from '../components/NavBar.vue'
 </script>
 
 <template>
   <main>
+    <NavBar />
     <TheWelcome />
   </main>
 </template>
